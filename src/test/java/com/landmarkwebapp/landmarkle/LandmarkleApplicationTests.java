48b559fa296f21796c0b09916f0f95b51138fef3
rohanj13 <rohan.australia@gmail.com>
2023-11-04 23:22:56 +1100

    transfer all files

---
+++ src/test/java/com/landmarkwebapp/landmarkle/LandmarkleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landmarkwebapp.landmarkle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandmarkleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
